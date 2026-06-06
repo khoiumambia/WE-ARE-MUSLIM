4b267e8390c1d4e9e0832ad36dd7c20a5a00cd93
Manzurul Ambia <manzurulambia@gmail.com>
2026-06-06 13:17:32 +0600

    Delete api directory

--- api/generate-hash.php
+++
@@ -1,8 +0,0 @@
-<?php
-// hash-generator.php
-echo "<h2>Password Hash Generator</h2>";
-echo "Password: admin123<br>";
-echo "Hash: " . password_hash('admin123', PASSWORD_DEFAULT) . "<br>";
-echo "<hr>";
-echo "Copy this hash and use it in your SQL INSERT statement.";
-?>
--- api/test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'success' => true, 
-    'message' => 'API is working',
-    'path' => __DIR__
-]);
-?>
